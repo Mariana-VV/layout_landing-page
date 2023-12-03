ab6f3c469d1ca475dbdefa89971a2667f3f60158
Mariana <marjanavv@gmail.com>
2023-12-03 11:46:25 +0100

    make build

---
+++ index.15f61248.js
@@ -0,0 +1,15 @@
+// const iconMenuEl = document.querySelector('.icon--menu');
+// iconMenuEl.addEventListener('click', () => {
+//   const bodyEl = document.querySelector('body');
+//   if (bodyEl.classList.contains('over')) {
+//     bodyEl.classList.remove('over');
+//   } else {
+//     bodyEl.classList.add('over');
+//   }
+// });
+// .over {
+//   overflow: hidden;
+// }
+//# sourceMappingURL=index.15f61248.js.map
+
+//# sourceMappingURL=index.15f61248.js.map
